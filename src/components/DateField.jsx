daee103a98f13c30b9e279996a04673fa0b5275d
anupammaiti10 <maitianupam567@gmail.com>
2025-04-17 00:21:16 +0530

    create date and button jsx file

---
+++ src/components/DateField.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function DateField({ label, date,setDateValue }) {
+    const handleDateValue=()=>{
+        const selectedDate = e.target.value;
+        setDateValue(selectedDate);
+    }
+  return (
+    <div className="w-full">
+      <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
+        {label}
+      </label>
+      <input
+        type="date"
+        value={date}
+        onChange={handleDateValue}
+        className="w-full px-4 py-2 border rounded-md shadow-sm bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+    </div>
+  );
+}
+
+export default DateField;
